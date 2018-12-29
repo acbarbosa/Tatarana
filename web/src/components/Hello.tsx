765e7ad5438917e25a027b8df384e172a53cd702
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2018-12-28 22:19:39 -0200

    Configures usage of CSS Modules with Scss files

--- web/src/components/Hello.tsx
+++ web/src/components/Hello.tsx
@@ -1,7 +1,8 @@
 import * as React from "react"
+import * as styles from "./Hello.scss"
 
 export class Hello extends React.Component<{}, {}> {
     render() {
-        return <h1>Tatarana Project!</h1>;
+        return <h1 className={styles.title}>Tatarana Project!</h1>;
     }
 }
--- web/webpack.config.js
+++ web/webpack.config.js
@@ -9,17 +9,45 @@ module.exports = {
     devtool: "source-map",
 
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
+        extensions: [".ts", ".tsx", ".js", ".json", ".scss"]
     },
 
     module: {
         rules: [
             // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+            {
+                test: /\.tsx?$/,
+                loader: "awesome-typescript-loader"
+            },
 
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    { loader: "style-loader" },
+                    {
+                        loader: "typings-for-css-modules-loader",
+                        options: {
+                            modules: true,
+                            namedExport: true,
+                            camelCase: true,
+                            localIdentName: '[name]__[local]___[hash:base64:5]'
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
         ]
     }
 };
