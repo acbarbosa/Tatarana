3ad209fc676ebd34997e073263c5d5d7f0406ea3
Alexandre Corrêa Barbosa <abarbos@thoughtworks.com>
2018-12-29 22:09:42 -0200

    Configures TSLint and fixes linter errors

--- web/src/components/Hello.tsx
+++ web/src/components/Hello.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import * as styles from "./Hello.scss"
 
 export class Hello extends React.Component<{}, {}> {
-    render() {
-        return <h1 className={styles.title}>Tatarana Project!</h1>;
+    public render() {
+        return <h1 className={styles.title}>Tatarana Project!</h1>
     }
 }
